df312c465aff493681ef4003c410c3a17cb98960
Michael <spireflux@gmail.com>
2016-02-13 19:51:42 -0500

    Checkpoint commit

--- public/js/mvpController.js
+++ public/js/mvpController.js
@@ -1,9 +1,32 @@
-    angular.module('mvp-app', [])
-        .controller('mvpController', function($scope, $http) {
-            $http.get('/notes').
-            success(function(data) {
-                $scope.notes = data;
-                console.log(data);
-            });
-        });
+// var mvpController = angular.module('mvpApp', []);
 
+// mvpController.controller('MvpController', [ '$scope', function($scope, $http) {
+
+//     $http.get('/notes').
+//         success(function(data) {
+//             $scope.notes = data;
+//                 console.log(data);
+//             });
+
+
+//     // $scope.update = function() {
+//     // };
+
+// }]);
+
+
+var mvpController = angular.module('mvpApp', []);
+
+mvpController.controller('MvpController', function($scope, $http) {
+    $http.get('/notes').
+        success(function(data) {
+            $scope.notes = data;
+    });
+
+    $scope.updateOnBlur = function() {
+        console.log("this is a test");
+        console.log($scope.notes);
+    };
+
+
+});
