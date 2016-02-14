f2fcd617d7e66e1802b9a3f3141c405b115891bf
Michael <spireflux@gmail.com>
2016-02-13 20:07:10 -0500

    Angular updateOnBlur in progress
    update on blur is working out nicely.  Still some work to be done
    with getting the proper id and data, then submitting all that
    to the backend, then doing a database push, but it seems mostly
    straightforward.

--- public/js/mvpController.js
+++ public/js/mvpController.js
@@ -1,31 +1,21 @@
-// var mvpController = angular.module('mvpApp', []);
-
-// mvpController.controller('MvpController', [ '$scope', function($scope, $http) {
-
-//     $http.get('/notes').
-//         success(function(data) {
-//             $scope.notes = data;
-//                 console.log(data);
-//             });
-
-
-//     // $scope.update = function() {
-//     // };
-
-// }]);
-
-
 var mvpController = angular.module('mvpApp', []);
 
 mvpController.controller('MvpController', function($scope, $http) {
+
+    // get the initial data from the server
     $http.get('/notes').
         success(function(data) {
             $scope.notes = data;
     });
 
-    $scope.updateOnBlur = function() {
-        console.log("this is a test");
-        console.log($scope.notes);
+    // update the data in place
+    $scope.updateOnBlur = function(id) {
+        console.log(id);
+
+        // send the updated data to the backend to be saved!
+        // angular - http.put
+        // $http.put('/note/:id', data );
+
     };
 
 
