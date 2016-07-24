1f7568e7b3f79842e2d6680dc1dfd832d7ad8b05
Michael <spireflux@gmail.com>
2016-07-24 16:33:35 -0400

    Finished linting

--- config/passport.js
+++ config/passport.js
@@ -8,30 +8,29 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
 });
 
 // Sign in using Email and Password.
-passport.use(new LocalStrategy({usernameField: 'email'},
-  (email, password, done) => {
+passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
     User.findOne({email: email.toLowerCase()}, (err, user) => {
-      if (!user) {
-        return done(null, false, {msg: `Email ${email} not found.`});
-      }
-      user.comparePassword(password, (err, isMatch) => {
-        if (isMatch) {
-          return done(null, user);
+        if (!user) {
+            return done(null, false, {msg: `Email ${email} not found.`});
         }
-        return done(null, false, { msg: 'Invalid email or password.' });
-      });
+        user.comparePassword(password, (err, isMatch) => {
+            if (isMatch) {
+                return done(null, user);
+            }
+            return done(null, false, { msg: 'Invalid email or password.' });
+        });
     });
-  }));
+}));
 
 /**
  * OAuth Strategy Overview
@@ -50,71 +49,71 @@ passport.use(new LocalStrategy({usernameField: 'email'},
 
 // Sign in with Google.
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL: '/auth/google/callback',
-  passReqToCallback: true
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
 }, (req, accessToken, refreshToken, profile, done) => {
-  if (req.user) {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (existingUser) {
-        req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, (err, user) => {
-          user.google = profile.id;
-          user.tokens.push({ kind: 'google', accessToken });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.gender = user.profile.gender || profile._json.gender;
-          user.profile.picture = user.profile.picture || profile._json.image.url;
-          user.save((err) => {
-            req.flash('info', { msg: 'Google account has been linked.' });
-            done(err, user);
-          });
+    if (req.user) {
+        User.findOne({ google: profile.id }, (err, existingUser) => {
+            if (existingUser) {
+                req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+                done(err);
+            } else {
+                User.findById(req.user.id, (err, user) => {
+                    user.google = profile.id;
+                    user.tokens.push({ kind: 'google', accessToken });
+                    user.profile.name = user.profile.name || profile.displayName;
+                    user.profile.gender = user.profile.gender || profile._json.gender;
+                    user.profile.picture = user.profile.picture || profile._json.image.url;
+                    user.save(err => {
+                        req.flash('info', { msg: 'Google account has been linked.' });
+                        done(err, user);
+                    });
+                });
+            }
         });
-      }
-    });
-  } else {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
-        if (existingEmailUser) {
-          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
-          done(err);
-        } else {
-          const user = new User();
-          user.email = profile.emails[0].value;
-          user.google = profile.id;
-          user.tokens.push({ kind: 'google', accessToken });
-          user.profile.name = profile.displayName;
-          user.profile.gender = profile._json.gender;
-          user.profile.picture = profile._json.image.url;
-          user.save((err) => {
-            done(err, user);
-          });
-        }
-      });
-    });
-  }
+    } else {
+        User.findOne({ google: profile.id }, (err, existingUser) => {
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+            User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
+                if (existingEmailUser) {
+                    req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+                    done(err);
+                } else {
+                    const user = new User();
+                    user.email = profile.emails[0].value;
+                    user.google = profile.id;
+                    user.tokens.push({ kind: 'google', accessToken });
+                    user.profile.name = profile.displayName;
+                    user.profile.gender = profile._json.gender;
+                    user.profile.picture = profile._json.image.url;
+                    user.save(err => {
+                        done(err, user);
+                    });
+                }
+            });
+        });
+    }
 }));
 
 // Login Required middleware.
 exports.isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
 };
 
 // Authorization Required middleware.
 exports.isAuthorized = (req, res, next) => {
-  const provider = req.path.split('/').slice(-1)[0];
+    const provider = req.path.split('/').slice(-1)[0];
 
-  if (_.find(req.user.tokens, {kind: provider})) {
-    next();
-  } else {
-    res.redirect(`/auth/${provider}`);
-  }
+    if (_.find(req.user.tokens, {kind: provider})) {
+        next();
+    } else {
+        res.redirect(`/auth/${provider}`);
+    }
 };
